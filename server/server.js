cbf7e14c9e36edb85709996af21ceee9a6edd4cd
Kawthar & Hanin <7223300558@students.liu.edu.lb>
2025-12-27 23:16:55 +0200

    add root route

--- server/server.js
+++ server/server.js
@@ -14,6 +14,10 @@ app.use(cors({
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.get("/", (req, res) => {
+  res.send("API is running ✅");
+});
+
 
 const uploadsDir = path.join(__dirname, "uploads");
 if (!fs.existsSync(uploadsDir)) {
