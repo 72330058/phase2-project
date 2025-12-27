974577435edaa79a2e29f30ed9b4cc093d107203
Kawthar & Hanin <7223300558@students.liu.edu.lb>
2025-12-28 01:04:55 +0200

    fix mysql2 promise pool

--- server/db.js
+++ server/db.js
@@ -1,4 +1,4 @@
-const mysql = require("mysql2");
+const mysql = require("mysql2/promise");
 
 function poolFromPublicUrl(publicUrl) {
   const u = new URL(publicUrl);
@@ -8,31 +8,18 @@ function poolFromPublicUrl(publicUrl) {
     port: Number(u.port || 3306),
     user: decodeURIComponent(u.username),
     password: decodeURIComponent(u.password),
-    database: u.pathname.replace("/", ""),
+    database: u.pathname.replace("/", ""), // <-- learnhub
     ssl: { rejectUnauthorized: false },
-
     waitForConnections: true,
     connectionLimit: 10,
     connectTimeout: 20000,
-    enableKeepAlive: true,
-    keepAliveInitialDelay: 10000,
   });
 }
 
-const db = process.env.MYSQL_PUBLIC_URL
-  ? poolFromPublicUrl(process.env.MYSQL_PUBLIC_URL)
-  : mysql.createPool({
-      host: process.env.MYSQLHOST,
-      user: process.env.MYSQLUSER,
-      password: process.env.MYSQLPASSWORD,
-      database: process.env.MYSQLDATABASE,
-      port: Number(process.env.MYSQLPORT || 3306),
-      ssl: { rejectUnauthorized: false },
-      waitForConnections: true,
-      connectionLimit: 10,
-      connectTimeout: 20000,
-      enableKeepAlive: true,
-      keepAliveInitialDelay: 10000,
-    });
+if (!process.env.MYSQL_PUBLIC_URL) {
+  throw new Error("MYSQL_PUBLIC_URL is missing");
+}
+
+const db = poolFromPublicUrl(process.env.MYSQL_PUBLIC_URL);
 
 module.exports = db;
